6fefe356ea1724df433cbf6f4002eca640dc4bab
Mikael Videll <widell79@gmail.com>
2021-02-11 21:55:53 +0100

    Added NavBar

--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -4,20 +4,24 @@ import Movies from "./movies";
 import Customers from "./customers";
 import Rentals from "./rentals";
 import NotFound from "./notFound";
+import NavBar from "./navBar";
 
 class MainContainer extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
