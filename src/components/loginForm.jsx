9b4d1fe5c79c38b1ff0ee30f5b2f2011c81a2340
Mikael Videll <widell79@gmail.com>
2021-02-12 23:09:23 +0100

    Handling Input State

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
   };
   render() {
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
+          <div className="form-group col-md-6">
             <label htmlFor="username">Username</label>
             <input
               autoFocus
+              value={this.state.account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
-          <div className="form-group">
+          <div className="form-group col-md-6">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={this.state.account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
