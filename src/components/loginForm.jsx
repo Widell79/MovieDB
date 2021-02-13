a0dda5b0e55d2a84baf1cb118fa75d8cb1309675
Mikael Videll <widell79@gmail.com>
2021-02-13 15:37:15 +0100

    Added Basic Validation on Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,10 +4,27 @@ import Input from "./input";
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
   };
 
   handleChange = (e) => {
@@ -22,15 +39,17 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
-            value={this.state.account.password}
-            label="Password"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="password"
             value={this.state.account.username}
             label="Username"
             onChange={this.handleChange}
+            error={this.state.errors.username}
+          />
+          <Input
+            name="password"
+            value={this.state.account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={this.state.errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
