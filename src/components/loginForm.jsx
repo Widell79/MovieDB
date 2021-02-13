148b4b2129239be87e52b469cf982db41f58764e
Mikael Videll <widell79@gmail.com>
2021-02-13 14:49:30 +0100

    Added Reusable Input Component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./input";
 
 class LoginForm extends Component {
   state = {
@@ -19,29 +20,19 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group col-md-6">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              value={this.state.account.username}
-              onChange={this.handleChange}
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group col-md-6">
-            <label htmlFor="password">Password</label>
-            <input
-              value={this.state.account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            name="username"
+            value={this.state.account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={this.state.account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
