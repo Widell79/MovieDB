8b41ab56512f0f4de85ab6c8e4ee930db7495372
Mikael <widell79@gmail.com>
2021-02-16 17:50:47 +0100

    Added Sentry Logging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import logger from "./services/logServices";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+logger.init();
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/logServices.js
@@ -0,0 +1,18 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn:
+      "https://119d73df802d41419ebca6309d65cda3@o525429.ingest.sentry.io/5639513",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
+
+export default {
+  init,
+};
