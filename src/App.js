9c48386d9f78c08ccad3c49e695a67b47ba75f35
Mikael Videll <widell79@gmail.com>
2021-02-17 19:06:31 +0100

    Fixed import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import MainContainer from "./components/mainContainer";
 
 function App() {
